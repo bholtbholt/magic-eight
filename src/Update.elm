b58c3e5ee2f2095626fc7bf16bdb6bd66c30f02f
Brian Holt <bholtbholt@icloud.com>
2018-03-01 12:28:10 -0800

    Convert from beginnerProgram to Program

--- src/Update.elm
+++ src/Update.elm
@@ -3,8 +3,8 @@ module Update exposing (update)
 import Types exposing (..)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         BasicUpdate ->
-            model
+            ( model, Cmd.none )
